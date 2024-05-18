5896a3748f4af20a08424c8de1609f00b285ee69
Daniil Sazanovich <sazdaniil2409@gmail.com>
2024-05-18 17:56:59 +0200

    developments App.Js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
+import SignInModal from './components/SignInModal';
+import SignUpModal from './components/SignUpModal';
 
-function App() {
+const App = () => {
+  const [isSignInOpen, setIsSignInOpen] = useState(false);
+  const [isSignUpOpen, setIsSignUpOpen] = useState(false);
+
+  const toggleSignIn = () => setIsSignInOpen(!isSignInOpen);
+  const toggleSignUp = () => setIsSignUpOpen(!isSignUpOpen);
+
   return (
     <div className="App">
-     
+      <button onClick={toggleSignIn}>Sign In</button>
+      <button onClick={toggleSignUp}>Sign Up</button>
+
+      {isSignInOpen && <SignInModal onClose={toggleSignIn} />}
+      {isSignUpOpen && <SignUpModal onClose={toggleSignUp} />}
     </div>
   );
-}
+};
 
 export default App;
